b27bd2c750ab13ed577e90290f0f9499542389d1
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-09-20 05:26:14 +0200

    add subnav to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,49 @@
 <script setup>
-import { RouterLink} from 'vue-router'
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+import SubNavVue from './SubNav.vue'
 import SearchIcon from './icons/IconSearch.vue'
 import UserIcon from './icons/IconUser.vue'
+import SubNav from './SubNav.vue'
 
-const mainLinks = ['watch', 'discover', 'buy', 'rewards', 'myDstv', 'help']
+const activeLink = ref(0)
+
+const links = [
+    { main: 'watch', sub: ['stream-now', 'live-tv', 'tv-shows', 'movies', 'sport', 'kids', 'tv-guide', 'boxOffice', 'showmax'] },
+    { main: 'discover', sub: ['tv-guide', 'highlights', 'loving-local', 'best-of-international', 'kids', 'behind-the-screens', 'sport', 'news'] },
+    { main: 'buy' },
+    { main: 'rewards' },
+    { main: 'myDstv' },
+    { main: 'help', sub: ['fix-errors', 'how-to-watch', 'how-to-play', 'find-an-installer', 'find-an-agency', 'faqs', 'contact-us'] }
+]
 
 </script>
     
 <template>
-    <header class="bg-black flex justify-between text-gray-300 px-10 py-8 items-center">
+    <header class="bg-black flex justify-between text-gray-300 px-10 py-5 items-center">
         <div class="flex items-center">
             <RouterLink to="/">
                 <img alt="" class="" src="@/assets/images/logo.svg" width="125" />
             </RouterLink>
             <div class="block h-5 w-5 mx-5 grid grid-cols-3 grid-rows-3 gap-1 self-center group">
-                <span v-for="n in 9" class="w-1 h-1 bg-gray-500 group-hover:bg-gray-200"></span>
+                <span v-for="n in 9" class="w-1 h-1 bg-gray-500 group-hover:bg-gray-200 transition"></span>
             </div>
 
             <nav aria-label="main navigation">
                 <ul class="flex font-light">
-                    <li v-for="link in mainLinks" class="mx-2 capitalize hover:text-blue-500">
-                        <RouterLink :to="`/${link}`">{{link}}</RouterLink>
+                    <li v-for="link in links" @click="activeLink = link"
+                        class="ss-right mx-2 capitalize hover:text-blue-500 transition">
+                        <RouterLink :to="`/${link.main}`">{{link.main}}</RouterLink>
                     </li>
                 </ul>
             </nav>
         </div>
 
         <nav aria-label="sub navigation" class="flex items-center font-light">
-            <SearchIcon class="w-7 mx-2 hover:text-blue-500" />
-            <UserIcon class="w-8 mx-2 hover:text-blue-500" />
-            <RouterLink to="/signin" class="mx-2 capitalize hover:text-blue-500">sign in</RouterLink>
+            <SearchIcon class="w-7 mx-2 hover:text-blue-500 transition"/>
+            <UserIcon class="w-8 mx-2 hover:text-blue-500 transition"/>
+            <RouterLink to="/signin" class="mx-2 capitalize hover:text-blue-500 transition">sign in</RouterLink>
         </nav>
     </header>
+    <SubNav v-if="activeLink.sub" :active-link="activeLink"></SubNav>
 </template>
---
+++ src/components/SubNav.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+    activeLink: {}
+})
+</script>
+
+<template>
+    <nav class="bg-gray-500 bg-opacity-30 ss-bottom-fast py-2 text-white">
+        <ul class="flex justify-center">
+            <li v-for="subLink in activeLink.sub"
+                class="mx-3 capitalize hover:text-blue-500 transition">
+                <RouterLink :to="`/${this.activeLink.main}/${subLink}`">{{subLink}}</RouterLink>
+            </li>
+        </ul>
+    </nav>
+</template>
